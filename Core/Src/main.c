2a45799b5872330da7a20de927adebd0025389ef
lashkoag <lashko-ag@energos.su>
2020-07-08 11:56:18 +0600

    program recive through uart commands and running its

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -104,10 +104,15 @@ static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN 0 */
 void get_sleep();
 void blink(uint8_t, uint32_t);
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *);
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *);
 
-int8_t FLAG_INTERRUPT_EVENT = 0;
-int8_t flag_btn_pressed = 0;
-int8_t flag_get_UART=1;
+int8_t flag_btn_irq = 0;
+int8_t flag_btn_user_pressed = 0;
+int8_t flag_get_UART=0;
+int8_t flag_need_sleep = 1;
+
+int8_t flag_uart_getBlink=0;
 
 int8_t MSV[sizeMSV]={10, };
 
@@ -124,57 +129,169 @@ uint16_t config=0;
 uint16_t calib=0;
 uint16_t die_id=0;
 
-uint8_t rx_uart[10];
-uint8_t tx_uart[10]={0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+uint8_t rx_uart[10]={0, };
+uint8_t tx_uart[10]={10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
 
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
-  
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* USER CODE BEGIN Init */
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE END Init */
+	/* USER CODE BEGIN Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* USER CODE END Init */
 
-  /* USER CODE BEGIN SysInit */
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE END SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_USART2_UART_Init();
-  MX_USART1_UART_Init();
-  /* USER CODE BEGIN 2 */
+	/* USER CODE END SysInit */
+
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_USART2_UART_Init();
+	MX_USART1_UART_Init();
+	/* USER CODE BEGIN 2 */
 	//для отладки
 	blink(6, 100);
-	//get_sleep();
- HAL_UART_Transmit(&huart1, tx_uart, 10, 100);
+	//	 get_sleep();
+	// HAL_UART_Transmit(&huart1, tx_uart, 10, 100);
+	__HAL_UART_ENABLE_IT(&huart1, UART_IT_RXNE);
 
-  /* USER CODE END 2 */
- 
- 
+	HAL_UART_Receive_IT (&huart1, (uint8_t*)rx_uart, 10);
+	HAL_UART_Transmit_IT (&huart1, (uint8_t*)tx_uart, 10);
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+
+	/* USER CODE END 2 */
+
+
+
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1)
 	{
+		//отправляем в сон
+		if(flag_need_sleep){
+			get_sleep();
+      //выход после режима
+			flag_need_sleep=0;
+		}
 
+		//если произошло прерывание по кнопке BTN_IRQ
+		if(flag_btn_irq){
+			//необходимо проверить состояние кнопки на удержание BTN_USER
+			//(BTN_USER и BTN_IRQ - физически это одна и та же кнопка, выводы дублируются, так как на ней и режим прерывания и обычный )
+			HAL_Delay(1900);
+			if(HAL_GPIO_ReadPin(BTN_USER_PORT, BTN_USER_PIN) == GPIO_PIN_RESET){
+        //кнопка удерживается какой то период времени
+				flag_btn_user_pressed=1;
+			}
+
+      //кнопку удерживали, необходимо вкл прибор
+			if(flag_btn_user_pressed){
+				//включаем пин, отвечающий за глобальное управление питанием
+				HAL_GPIO_WritePin(PIN_GLOBAL_POWER_PORT, PIN_GLOBAL_POWER_PIN, GPIO_PIN_SET);
+				blink(6,50);  //для отладки
+				//LED сигнализирующий, что питание подано
+				HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
+				flag_btn_user_pressed = 0;
+				memset(&rx_uart[0], 0x00, 10);
+				//разрешаем работу по UART`у
+				flag_get_UART=1;
+
+				while(flag_get_UART){
+					HAL_UART_Receive_IT (&huart1, (uint8_t*)rx_uart, 10);
+					//данный буффер собирается в обработчике прерываний
+					int8_t action=rx_uart[0];
+
+					switch (action)
+					{
+					//команда получения данных с INA
+					case 0x02:
+						blink(6,50);
+						//get_data_ina();
+						//обнуление массива rx_uart
+						// memset(&rx_uart[0], 0x00, 10);
+						break;
+						//команда выключения
+					case 0x03:
+						blink(6, 1000);
+
+						flag_get_UART = 0;
+						flag_btn_user_pressed = 0;
+						flag_btn_irq=0;
+						flag_need_sleep=1;
+
+						HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET); // для отладки - удалить
+
+						//перед выключением питания необходим таймаут
+						//для корректного выключения master`a
+						// HAL_Delay(60000);
+						HAL_Delay(6000);
+
+						//пин управления питанием в LOW
+						HAL_GPIO_WritePin(PIN_GLOBAL_POWER_PORT, PIN_GLOBAL_POWER_PIN, GPIO_PIN_RESET);
+						//светодиод индикации питания выкл
+						HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_RESET);
+						break;
+
+					default:
+						//другие команды игнорируются
+						break;
+					}
+
+					//обнуление массива rx_uart
+					memset(&rx_uart[0], 0x00, 10);
+
+					// if(flag_uart_getBlink){
+					//   blink(6,50);
+					//   flag_get_UART=0;
+					// }
+				}
+
+			}else{
+				flag_need_sleep=1;
+				flag_btn_irq=0;
+			}
+
+		}
+
+		// HAL_UART_Receive_IT (&huart1, (uint8_t*)rx_uart, 10);
+		// if(huart1.RxXferCount == 0){
+		//   HAL_UART_Transmit_IT (&huart1, tx_uart, 10);
+		// }
+
+
+		// blink(10, 50);
+		// if(flag_get_UART){
+		// 	flag_get_UART=0;
+		// 	// HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
+		// 	blink(10, 50);
+		// 	// memset(&rx_uart[0], 0x00, 10);
+		// 	HAL_UART_Transmit_IT (&huart1, (uint8_t*)tx_uart, 10);
+		// 	__HAL_UART_ENABLE_IT(&huart1, UART_IT_RXNE);
+
+		// 	for(uint8_t i =0; i<10; ++i){
+		// 		rx_uart[i]=0;
+		// 	}
+		// }
+		//    HAL_UART_Transmit_IT (&huart1, tx_uart, 10);
+		//    HAL_Delay(2000);
+		// flag_need_sleep=0;
 		// if(HAL_GPIO_ReadPin(BTN_USER_PORT, BTN_USER_PIN) == GPIO_PIN_RESET){
 		//  HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
 		// }else{
@@ -187,208 +304,182 @@ int main(void)
 		//	  }else{
 		//		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_RESET);
 		//	  }
+
 		//произошло короткое нажатие кнопки
-		if(FLAG_INTERRUPT_EVENT){
 
-			//необходимо проверить состояние кнопки на удержание
-			HAL_Delay(1900);
-			if(HAL_GPIO_ReadPin(BTN_USER_PORT, BTN_USER_PIN) == GPIO_PIN_RESET){
-				flag_btn_pressed=1;
-				FLAG_INTERRUPT_EVENT=0;
-			}
+		/* USER CODE END WHILE */
 
-			if(flag_btn_pressed){
-				//включаем пин, отвечающий за глобальное управление питанием
-				HAL_GPIO_WritePin(PIN_GLOBAL_POWER_PORT, PIN_GLOBAL_POWER_PIN, GPIO_PIN_SET);
-
-				//LED сигнализирующий, что питание подано
-				HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
-				flag_btn_pressed = 0;
-			}else{
-
-			}
-
-			// if(0){
-			FLAG_INTERRUPT_EVENT=0;
-			// HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
-			// HAL_Delay (5000);
-			// HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_RESET);
-			// HAL_Delay (5000);
-			// blink(6, 100);
-		}
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+		/* USER CODE BEGIN 3 */
 	}
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage 
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-  /** Initializes the CPU, AHB and APB busses clocks 
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 100;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB busses clocks 
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+	/** Initializes the CPU, AHB and APB busses clocks
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLM = 8;
+	RCC_OscInitStruct.PLL.PLLN = 100;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 4;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	/** Initializes the CPU, AHB and APB busses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+	{
+		Error_Handler();
+	}
 }
 
 /**
-  * @brief USART1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART1_UART_Init(void)
 {
 
-  /* USER CODE BEGIN USART1_Init 0 */
+	/* USER CODE BEGIN USART1_Init 0 */
 
-  /* USER CODE END USART1_Init 0 */
+	/* USER CODE END USART1_Init 0 */
 
-  /* USER CODE BEGIN USART1_Init 1 */
+	/* USER CODE BEGIN USART1_Init 1 */
 
-  /* USER CODE END USART1_Init 1 */
-  huart1.Instance = USART1;
-  huart1.Init.BaudRate = 9600;
-  huart1.Init.WordLength = UART_WORDLENGTH_8B;
-  huart1.Init.StopBits = UART_STOPBITS_1;
-  huart1.Init.Parity = UART_PARITY_NONE;
-  huart1.Init.Mode = UART_MODE_TX_RX;
-  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART1_Init 2 */
+	/* USER CODE END USART1_Init 1 */
+	huart1.Instance = USART1;
+	huart1.Init.BaudRate = 9600;
+	huart1.Init.WordLength = UART_WORDLENGTH_8B;
+	huart1.Init.StopBits = UART_STOPBITS_1;
+	huart1.Init.Parity = UART_PARITY_NONE;
+	huart1.Init.Mode = UART_MODE_TX_RX;
+	huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+	if (HAL_UART_Init(&huart1) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	/* USER CODE BEGIN USART1_Init 2 */
 
-  /* USER CODE END USART1_Init 2 */
+	/* USER CODE END USART1_Init 2 */
 
 }
 
 /**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART2_UART_Init(void)
 {
 
-  /* USER CODE BEGIN USART2_Init 0 */
+	/* USER CODE BEGIN USART2_Init 0 */
 
-  /* USER CODE END USART2_Init 0 */
+	/* USER CODE END USART2_Init 0 */
 
-  /* USER CODE BEGIN USART2_Init 1 */
+	/* USER CODE BEGIN USART2_Init 1 */
 
-  /* USER CODE END USART2_Init 1 */
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 9600;
-  huart2.Init.WordLength = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART2_Init 2 */
+	/* USER CODE END USART2_Init 1 */
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 9600;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.Parity = UART_PARITY_NONE;
+	huart2.Init.Mode = UART_MODE_TX_RX;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+	if (HAL_UART_Init(&huart2) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	/* USER CODE BEGIN USART2_Init 2 */
 
-  /* USER CODE END USART2_Init 2 */
+	/* USER CODE END USART2_Init 2 */
 
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_GPIOH_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOD_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_RESET);
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : B1_Pin */
-  GPIO_InitStruct.Pin = B1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
-  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+	/*Configure GPIO pin : B1_Pin */
+	GPIO_InitStruct.Pin = B1_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+	GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+	HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PC14 */
-  GPIO_InitStruct.Pin = GPIO_PIN_14;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+	/*Configure GPIO pin : PC14 */
+	GPIO_InitStruct.Pin = GPIO_PIN_14;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PC0 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+	/*Configure GPIO pin : PC0 */
+	GPIO_InitStruct.Pin = GPIO_PIN_0;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+	/*Configure GPIO pin : LD2_Pin */
+	GPIO_InitStruct.Pin = LD2_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PD2 */
-  GPIO_InitStruct.Pin = GPIO_PIN_2;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
-  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+	/*Configure GPIO pin : PD2 */
+	GPIO_InitStruct.Pin = GPIO_PIN_2;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+	HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+	/* EXTI interrupt init*/
+	HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+	HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 }
 
@@ -397,7 +488,8 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
 	if(GPIO_Pin== GPIO_PIN_13) {
 		//HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_SET);
-		FLAG_INTERRUPT_EVENT=1;
+		flag_btn_irq=1;
+		flag_need_sleep = 0;
 	}else{
 		__NOP();
 	}
@@ -422,34 +514,53 @@ void blink(uint8_t counter_blink, uint32_t t_ms)
 		HAL_Delay (t_ms);
 	}
 }
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	//	__NOP();
+	// HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
+	if(huart == &huart1){
+		// flag_get_UART=1;        // что-то делаем
+		flag_uart_getBlink =1;
+		// blink(6, 50);
+	}
+	//	flag_get_UART=1;
+
+
+}
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
+{
+	__NOP();
+}
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
+	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 { 
-  /* USER CODE BEGIN 6 */
+	/* USER CODE BEGIN 6 */
 	/* User can add his own implementation to report the file name and line number,
-     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+ tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	/* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
 
