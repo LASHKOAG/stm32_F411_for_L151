a5fb5f22e35d3b5f9ebb248a2a29a0fa994dd790
lashkoag <lashko-ag@energos.su>
2020-07-08 15:38:34 +0600

    cleaning code

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -45,7 +45,7 @@
 #define PIN_GLOBAL_POWER_PORT  GPIOC
 #define PIN_GLOBAL_POWER_PIN   GPIO_PIN_0
 
-#define sizeMSV  100
+#define size_rx_uart  12
 
 typedef struct 
 {
@@ -106,6 +106,8 @@ void get_sleep();
 void blink(uint8_t, uint32_t);
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *);
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *);
+void send_message (tcp_packet_t*);
+void get_data_ina();
 
 int8_t flag_btn_irq = 0;
 int8_t flag_btn_user_pressed = 0;
@@ -114,9 +116,9 @@ int8_t flag_need_sleep = 1;
 
 int8_t flag_uart_getBlink=0;
 
-int8_t MSV[sizeMSV]={10, };
-
-int8_t counters = 0;
+//int8_t MSV[sizeMSV]={10, };
+//
+//int8_t counters = 0;
 
 float current_mA = 0.0f;
 int16_t current_reg = 0;
@@ -129,8 +131,8 @@ uint16_t config=0;
 uint16_t calib=0;
 uint16_t die_id=0;
 
-uint8_t rx_uart[10]={0, };
-uint8_t tx_uart[10]={10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+uint8_t rx_uart[12]={0, };
+//uint8_t tx_uart[10]={10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
 
 /* USER CODE END 0 */
 
@@ -165,20 +167,15 @@ int main(void)
 	MX_GPIO_Init();
 	MX_USART2_UART_Init();
 	MX_USART1_UART_Init();
+
 	/* USER CODE BEGIN 2 */
+
 	//для отладки
 	blink(6, 100);
-	//	 get_sleep();
-	// HAL_UART_Transmit(&huart1, tx_uart, 10, 100);
+
 	__HAL_UART_ENABLE_IT(&huart1, UART_IT_RXNE);
 
-	HAL_UART_Receive_IT (&huart1, (uint8_t*)rx_uart, 10);
-	HAL_UART_Transmit_IT (&huart1, (uint8_t*)tx_uart, 10);
-
-
 	/* USER CODE END 2 */
-
-
 
 	/* Infinite loop */
 	/* USER CODE BEGIN WHILE */
@@ -187,34 +184,36 @@ int main(void)
 		//отправляем в сон
 		if(flag_need_sleep){
 			get_sleep();
-      //выход после режима
+			//выход после режима
 			flag_need_sleep=0;
 		}
 
+		//произошло короткое нажатие кнопки - вызывает прерывание
 		//если произошло прерывание по кнопке BTN_IRQ
 		if(flag_btn_irq){
 			//необходимо проверить состояние кнопки на удержание BTN_USER
-			//(BTN_USER и BTN_IRQ - физически это одна и та же кнопка, выводы дублируются, так как на ней и режим прерывания и обычный )
+			//(BTN_USER и BTN_IRQ - физически это одна и та же кнопка, выводы дублируются на ещё один пин, так как на ней и режим прерывания и обычный )
 			HAL_Delay(1900);
 			if(HAL_GPIO_ReadPin(BTN_USER_PORT, BTN_USER_PIN) == GPIO_PIN_RESET){
-        //кнопка удерживается какой то период времени
+				//кнопка удерживается какой то период времени
 				flag_btn_user_pressed=1;
 			}
 
-      //кнопку удерживали, необходимо вкл прибор
+			//кнопку удерживали, необходимо вкл прибор
 			if(flag_btn_user_pressed){
 				//включаем пин, отвечающий за глобальное управление питанием
 				HAL_GPIO_WritePin(PIN_GLOBAL_POWER_PORT, PIN_GLOBAL_POWER_PIN, GPIO_PIN_SET);
 				blink(6,50);  //для отладки
 				//LED сигнализирующий, что питание подано
-				HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
+				//+++HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
 				flag_btn_user_pressed = 0;
-				memset(&rx_uart[0], 0x00, 10);
+
 				//разрешаем работу по UART`у
 				flag_get_UART=1;
 
 				while(flag_get_UART){
-					HAL_UART_Receive_IT (&huart1, (uint8_t*)rx_uart, 10);
+					HAL_UART_Receive_IT (&huart1, (uint8_t*)rx_uart, size_rx_uart);
+
 					//данный буффер собирается в обработчике прерываний
 					int8_t action=rx_uart[0];
 
@@ -223,11 +222,9 @@ int main(void)
 					//команда получения данных с INA
 					case 0x02:
 						blink(6,50);
-						//get_data_ina();
-						//обнуление массива rx_uart
-						// memset(&rx_uart[0], 0x00, 10);
+						get_data_ina();
 						break;
-						//команда выключения
+					//команда выключения
 					case 0x03:
 						blink(6, 1000);
 
@@ -255,12 +252,7 @@ int main(void)
 					}
 
 					//обнуление массива rx_uart
-					memset(&rx_uart[0], 0x00, 10);
-
-					// if(flag_uart_getBlink){
-					//   blink(6,50);
-					//   flag_get_UART=0;
-					// }
+					memset(&rx_uart[0], 0x00, 12);
 				}
 
 			}else{
@@ -269,43 +261,6 @@ int main(void)
 			}
 
 		}
-
-		// HAL_UART_Receive_IT (&huart1, (uint8_t*)rx_uart, 10);
-		// if(huart1.RxXferCount == 0){
-		//   HAL_UART_Transmit_IT (&huart1, tx_uart, 10);
-		// }
-
-
-		// blink(10, 50);
-		// if(flag_get_UART){
-		// 	flag_get_UART=0;
-		// 	// HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
-		// 	blink(10, 50);
-		// 	// memset(&rx_uart[0], 0x00, 10);
-		// 	HAL_UART_Transmit_IT (&huart1, (uint8_t*)tx_uart, 10);
-		// 	__HAL_UART_ENABLE_IT(&huart1, UART_IT_RXNE);
-
-		// 	for(uint8_t i =0; i<10; ++i){
-		// 		rx_uart[i]=0;
-		// 	}
-		// }
-		//    HAL_UART_Transmit_IT (&huart1, tx_uart, 10);
-		//    HAL_Delay(2000);
-		// flag_need_sleep=0;
-		// if(HAL_GPIO_ReadPin(BTN_USER_PORT, BTN_USER_PIN) == GPIO_PIN_RESET){
-		//  HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
-		// }else{
-		//  HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_RESET);
-		// }
-
-		//	  if( HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_0) == GPIO_PIN_SET ) {
-		//	      // на выводе PB12 высокий уровень, кнопка отжата
-		//		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_SET);
-		//	  }else{
-		//		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_RESET);
-		//	  }
-
-		//произошло короткое нажатие кнопки
 
 		/* USER CODE END WHILE */
 
@@ -373,7 +328,8 @@ static void MX_USART1_UART_Init(void)
 
 	/* USER CODE END USART1_Init 1 */
 	huart1.Instance = USART1;
-	huart1.Init.BaudRate = 9600;
+//	huart1.Init.BaudRate = 9600;
+	huart1.Init.BaudRate = 115200;
 	huart1.Init.WordLength = UART_WORDLENGTH_8B;
 	huart1.Init.StopBits = UART_STOPBITS_1;
 	huart1.Init.Parity = UART_PARITY_NONE;
@@ -518,20 +474,139 @@ void blink(uint8_t counter_blink, uint32_t t_ms)
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
 	//	__NOP();
-	// HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
 	if(huart == &huart1){
-		// flag_get_UART=1;        // что-то делаем
 		flag_uart_getBlink =1;
-		// blink(6, 50);
 	}
-	//	flag_get_UART=1;
-
-
 }
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
 {
 	__NOP();
+}
+
+// void send_message (tcp_packet_t* ans_struct, RawSerial* _pc){
+void send_message (tcp_packet_t* ans_struct){
+	uint16_t len_send=sizeof(ans_struct->command)+sizeof(ans_struct->length) + ans_struct->length;
+	// pc.putc(len_send);  //для отладки
+	const uint8_t sizeTempMSV = 200;
+	uint8_t TempMSV[sizeTempMSV];
+	memset(&TempMSV[0], 0x00, 200);
+	//собираем сообщение
+	uint8_t pos=0;
+	memcpy(&TempMSV[pos], &ans_struct->command, sizeof(ans_struct->command));
+	 pos+=sizeof(ans_struct->command);
+	memcpy(&TempMSV[pos], &ans_struct->length, sizeof(ans_struct->length));
+	 pos+=sizeof(ans_struct->length);
+	memcpy(&TempMSV[pos], &ans_struct->buff[0], ans_struct->length);
+
+	//send message
+	HAL_UART_Transmit_IT (&huart1, (uint8_t*)TempMSV, len_send-13);
+//	HAL_UART_Transmit(&huart1, (uint8_t*)&TempMSV[0], len_send-13, 100);
+}
+
+void get_data_ina(){
+	INA_230 ina;
+	memset(&ina, 0x00, sizeof(INA_230));
+	//================================================================================
+	ina.hz = 10;                    //для отладки
+	ina.current_mA = 10.7;          //для отладки
+	ina.power_W = 11.8;
+	ina.bus_voltage_V = 12.9;
+	ina.shunt_voltage_V = 13.10;
+	ina.current_reg = 14;
+	ina.config = 15;
+	ina.calib = 16;
+	ina.die_id = 17;
+	ina.addr = 18;
+
+	//работаем с INA-----------------------------------------------
+	// time_t seconds = time(NULL);
+	// read the data
+	// INA230 *ina230 =new  INA230(i2c);
+	// delete ina230;
+
+	// // void frequency(int hz);
+	// current_mA = read_current();
+	// current_reg = read_current_reg();
+	// // float read_current_by_shuntvolt(void);
+	// power_W = read_power();
+	// bus_voltage_V = read_bus_voltage();
+	// shunt_voltage_V = read_shunt_voltage();
+	// config = read_config();
+	// // uint16_t set_config(uint16_t cfg);
+	// calib = read_calb(void);
+	// die_id = read_die_id();
+	// uint16_t set_calb(uint16_t clb);
+	// uint8_t write_reg(uint8_t addr, uint8_t data);
+	// uint8_t read_reg(uint8_t addr);
+
+	// uint16_t read_mask_enable();
+
+	// uint16_t read_alert_limit();
+
+	// uint16_t set_mask_enable(uint16_t cfg);
+
+	// uint16_t set_alert_limit(uint16_t cfg);
+
+	// int16_t  get_shunt_res();
+	//--------------------------------------------------------------------------
+	//==========================================================================
+	//собрать сообщение для отправки
+	//==========================================================================
+	const uint8_t sizeBuffer = 100;
+	int8_t Buffer[sizeBuffer];
+	//заслать ответ
+	uint32_t res = SLAVE_SUCCESS_ANSWER;
+	tcp_packet_t ans;
+	uint8_t pos = 0;
+	uint32_t packet_command = 2;
+	memset(&ans, 0x00, sizeof(tcp_packet_t));
+	memset(&Buffer[0], 0x00, sizeBuffer );
+	ans.command = CMD_ANSWER;
+	ans.length = sizeof(ans.command)+sizeof(res)
+                		+sizeof(ina.hz)+sizeof(ina.current_mA)
+						+sizeof(ina.power_W)+sizeof(ina.bus_voltage_V)+sizeof(ina.shunt_voltage_V)
+						+sizeof(ina.current_reg)+sizeof(ina.config)+sizeof(ina.calib)
+						+sizeof(ina.die_id)+sizeof(ina.addr);  //+всё в пакете
+						// ans.length = sizeof(uint32_t)+sizeof(uint8_t)                        //sizeof(ans.command)+sizeof(res)
+	//             +sizeof(uint32_t)+sizeof(float)                         //sizeof(ina.hz)+sizeof(ina.current_mA)
+	//             +sizeof(float)+sizeof(float)+sizeof(float)              //sizeof(ina.power_W)+sizeof(ina.bus_voltage_V)+sizeof(ina.shunt_voltage_V)
+	//             +sizeof(int16_t)+sizeof(uint16_t)+sizeof(uint16_t)      //sizeof(ina.current_reg)+sizeof(ina.config)+sizeof(ina.calib)
+	//             +sizeof(uint16_t)+sizeof(uint8_t);  //+всё в пакете     //sizeof(ina.die_id)+sizeof(ina.addr);
+	// ans.length = sizeof(ans.command)+sizeof(res) + sizeof(INA_230);  //+всё в пакете
+
+	// pc.putc(ans.length);  //для отладки
+	// memcpy(&Buffer[pos], (char*)&packet.command, sizeof(packet.command));
+	memcpy(&Buffer[pos], (char*)&packet_command, sizeof(packet_command));
+	// pos+= sizeof(packet.command);
+	pos+= sizeof(packet_command);
+	memcpy(&Buffer[pos], (char*)&res, sizeof(res));
+	pos+=sizeof(res);
+	//+всё в пакете
+	memcpy(&Buffer[pos], (char*)&ina.hz, sizeof(ina.hz));
+	pos+=sizeof(ina.hz);
+	memcpy(&Buffer[pos], (char*)&ina.current_mA, sizeof(ina.current_mA));
+	pos+=sizeof(ina.current_mA);
+	memcpy(&Buffer[pos], (char*)&ina.power_W, sizeof(ina.power_W));
+	pos+=sizeof(ina.power_W);
+	memcpy(&Buffer[pos], (char*)&ina.bus_voltage_V, sizeof(ina.bus_voltage_V));
+	pos+=sizeof(ina.bus_voltage_V);
+	memcpy(&Buffer[pos], (char*)&ina.shunt_voltage_V, sizeof(ina.shunt_voltage_V));
+	pos+=sizeof(ina.shunt_voltage_V);
+	memcpy(&Buffer[pos], (char*)&ina.current_reg, sizeof(ina.current_reg));
+	pos+=sizeof(ina.current_reg);
+	memcpy(&Buffer[pos], (char*)&ina.config, sizeof(ina.config));
+	pos+=sizeof(ina.config);
+	memcpy(&Buffer[pos], (char*)&ina.calib, sizeof(ina.calib));
+	pos+=sizeof(ina.calib);
+	memcpy(&Buffer[pos], (char*)&ina.die_id, sizeof(ina.die_id));
+	pos+=sizeof(ina.die_id);
+	memcpy(&Buffer[pos], (char*)&ina.addr, sizeof(ina.addr));
+	pos+=sizeof(ina.addr);
+
+	pos=0;
+	ans.buff = &Buffer[0];
+	send_message(&ans);
 }
 /* USER CODE END 4 */
 
